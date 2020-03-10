d4b8e74e93efabf3813c8eee1e7432fa084be46e
admin <2272068516@qq.com>
2020-03-10 22:15:38 +0800

    设计后端架构

---
+++ ServiceNode/backEnd/src/main/java/com/dtask/DTask/DTaskApplication.java
@@ -0,0 +1,13 @@
+package com.dtask.DTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DTaskApplication.class, args);
+	}
+
+}
---
+++ ServiceNode/backEnd/src/test/java/com/dtask/DTask/DTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtask.DTask;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
